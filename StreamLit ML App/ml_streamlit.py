ab16a26aaaa6df469181bba1d773720dd421284f
NISHANT SUHAG <112072807+NishantSuhag02@users.noreply.github.com>
2025-04-10 00:38:24 +0530

    Added functionalities

--- StreamLit ML App/ml_streamlit.py
+++ StreamLit ML App/ml_streamlit.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import sklearn
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 
 @st.cache_data
 def load_data():
@@ -13,9 +17,28 @@ def load_data():
 
 df,target_name = load_data()
 
+model_choice = st.sidebar.selectbox(
+    "Choose a Classification Model",
+    ("Random Forest", "Support Vector Machine (SVM)", "K-Nearest Neighbors (KNN)")
+)
+
+
+# Initialize and train model based on user choice
+if model_choice == "Random Forest":
+    model = RandomForestClassifier()
+elif model_choice == "Support Vector Machine (SVM)":
+    model = SVC(probability=True)  
+elif model_choice == "K-Nearest Neighbors (KNN)":
+    model = KNeighborsClassifier()
+
+x = df.iloc[:, :-1]
+y = df['species']
+
 # Training Model
-model = RandomForestClassifier()
-model.fit(df.iloc[:,:-1], df['species'])
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
+model.fit(X_train, y_train)
+test_accuracy = model.score(X_test, y_test)
+
 
 # Creating a Slider using Streamlit
 st.sidebar.title('Input Features')
@@ -32,12 +55,36 @@ input_data=[[sepal_length,sepal_width,petal_length,petal_width]]
 prediction = model.predict(input_data)
 predicted_species = target_name[prediction[0]]
 
+models = {
+    "Random Forest": RandomForestClassifier(),
+    "SVM": SVC(probability=True),
+    "KNN": KNeighborsClassifier()
+}
+
+results = []
+
+for name, mdl in models.items():
+    mdl.fit(X_train, y_train)
+    y_pred = mdl.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    results.append({
+        "Model": name,
+        "Accuracy": f"{acc*100:.2f}%"
+    })
+
+results_df = pd.DataFrame(results)
+
 # Printing Predicted species
 st.title('Leaves Prediction Web App \n')
+st.write(f" Prediction Model used: **{model_choice}**")
 st.write(f'The predicted species is : **{predicted_species.upper()}**')
+st.write(f"Test Accuracy: **{test_accuracy*100:.2f}%**")
 st.markdown(f"""Entered leave dimensions are :-  
             - **Sepal Length** : {sepal_length}  
             - **Sepal Width** : {sepal_width}  
             - **Petal Length** : {petal_length}  
             - **Petal Width** : {petal_width}""")
 
+results_df.index = results_df.index + 1 
+st.subheader("Model Comparison")
+st.dataframe(results_df)
